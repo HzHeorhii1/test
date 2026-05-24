2c57d70e21de5a69a1a63bd043594aa3ec96cc34
hhuziuk <huziukwork@gmail.com>
2026-05-24 15:14:52 +0200

    feat(common): wire up public exports and add build/deploy scripts

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,1 +1,14 @@
-// @todo
+export { AggregateRootBase } from './ddd/aggregate-root.base';
+export { EntityBase } from './ddd/entity.base';
+export { ValueObject } from './ddd/value-object.base';
+export { DomainEventBase } from './ddd/domain-event.base';
+export { IRepository } from './ddd/repository.port';
+
+export { AppLoggerService } from './logger/logger.service';
+export { LoggerModule } from './logger/logger.module';
+
+export { HttpExceptionFilter } from './exceptions/http-exception.filter';
+export { GrpcExceptionFilter } from './exceptions/grpc-exception.filter';
+
+export { API_VERSION_HEADER, ApiVersionMiddleware } from './versioning/api-version.middleware';
+export { VersionedResponseMapper } from './versioning/versioned-response.mapper';
